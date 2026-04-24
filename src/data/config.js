f3aae1ce9001d87efdb04f67f2d04c81e6699db5
matan0810 <65176459+matan0810@users.noreply.github.com>
2026-04-24 18:05:33 +0300

    feat: add university constant to configuration

--- src/data/config.js
+++ src/data/config.js
@@ -1,3 +1,4 @@
 export const TEACHER = "פרופ׳ יורם לסט";
 export const COURSE_NAME = "חשבון אינפיניטסימלי 2";
 export const COURSE_NUMBER = "80132";
+export const UNIVERSITY = "האוניברסיטה העברית בירושלים";
